dd874425b93ed1868e2ed93ee9ddc02de472ed7e
Gabriel Ferro <gabrielferroroque@hotmail.com>
2022-11-18 01:21:59 +0000

    fix: removed child from global style because it do not accept child components.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './pages/App'
-import './index.css'
 import { GlobalStyle } from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyle>
-      <App />
-    </GlobalStyle>
+    <GlobalStyle />
+
   </React.StrictMode>
 )
