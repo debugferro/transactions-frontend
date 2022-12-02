17390b74263a4eec5f676c726ac6963b219a79bd
Gabriel Ferro <gabrielferroroque@hotmail.com>
2022-12-02 05:15:19 +0000

    feat: created PageCard, Title and SubTitled styled components (div, h1, h3)

--- src/styles/page.styles.js
+++ src/styles/page.styles.js
@@ -5,6 +5,28 @@ export const PageContainer = styled.div`
   height: 100%;
   justify-content: center;
 `;
+
+export const PageCard = styled.div`
+  display: flex;
+  flex-flow: column;
+  margin: 0 2rem;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 10px;
+  width: 90%;
+  align-self: center;
+  height: 80%;
+`;
+
+export const Title = styled.h1`
+  text-transform: capitalize;
+`;
+
+export const SubTitle = styled.h3`
+  color: var(--medium-gray);
+  text-transform: uppercase;
+`;
+
 export const ColorfulPill = styled.span`
   border-radius: 5px;
   background-color: ${(props) => `#${props.color || "7e7878"}`};
