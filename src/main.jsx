3c0a550de820376e021bb691bdf0061c3aedc848
Gabriel Ferro <gabrielferroroque@hotmail.com>
2022-12-02 00:41:36 +0000

    feat: added apollo client provider and app routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Global } from '@emotion/react'
-import { GlobalStyle } from './styles/global'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Global } from "@emotion/react";
+import { GlobalStyle } from "./styles/global";
+import { AppRoutes } from "./AppRoutes";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./services/transactions_backend/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Global styles={GlobalStyle} />
-
+    <ApolloProvider client={client}>
+      <AppRoutes />
+    </ApolloProvider>
   </React.StrictMode>
-)
+);
