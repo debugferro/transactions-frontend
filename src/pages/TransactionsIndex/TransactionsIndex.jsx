1d852466db8f21c944e79b1784543569046d5dfd
Gabriel Ferro <gabrielferroroque@hotmail.com>
2022-12-02 05:12:35 +0000

    feat: created TransactionsIndex page

--- src/pages/TransactionsIndex/TransactionsIndex.jsx
+++ src/pages/TransactionsIndex/TransactionsIndex.jsx
@@ -1,4 +1,20 @@
+import TransactionsTable from "./components/TransactionsTable/TransactionsTable";
+import { SearchBar } from "./components/SearchBar/SearchBar";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
+import { TableContainer } from "./TransactionsIndex.styles";
+import { PageContainer } from "../../styles/page.styles";
+
 function TransactionsIndex() {
+  return (
+    <PageContainer>
+      <TableContainer>
+        <TransactionsProvider>
+          <SearchBar />
+          <TransactionsTable />
+        </TransactionsProvider>
+      </TableContainer>
+    </PageContainer>
+  );
 }
 
 export default TransactionsIndex;
