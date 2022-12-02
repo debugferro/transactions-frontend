b49df0c59fe3fff53106914827a45da3568a7908
Gabriel Ferro <gabrielferroroque@hotmail.com>
2022-12-02 05:11:13 +0000

    feat: added PageContainer styled div

--- src/styles/page.styles.js
+++ src/styles/page.styles.js
@@ -1,4 +1,10 @@
 import styled from "@emotion/styled";
+
+export const PageContainer = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: center;
+`;
 export const ColorfulPill = styled.span`
   border-radius: 5px;
   background-color: ${(props) => `#${props.color || "7e7878"}`};
