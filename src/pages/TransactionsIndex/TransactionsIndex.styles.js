2528401d07eca6bbfb723fd95878ce7ec52a3917
Gabriel Ferro <gabrielferroroque@hotmail.com>
2022-12-02 05:11:46 +0000

    feat: added TableContainer styled div

---
+++ src/pages/TransactionsIndex/TransactionsIndex.styles.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const TableContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  margin: 0 2rem;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 10px;
+  width: 90%;
+  align-self: center;
+  height: 80%;
+`;
