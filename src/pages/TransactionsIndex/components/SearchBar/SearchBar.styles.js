26de117af1c1f734b12f1fa7fc18a829af38c05a
Gabriel Ferro <gabrielferroroque@hotmail.com>
2022-12-02 05:12:17 +0000

    feat: created search bar styles

---
+++ src/pages/TransactionsIndex/components/SearchBar/SearchBar.styles.js
@@ -0,0 +1,18 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const SearchContainer = styled.div`
+  display: flex;
+`;
+
+export const textFieldCSS = css`
+  width: 40%;
+`;
+
+export const calendarFieldCSS = css`
+  width: 30%;
+`;
+
+export const selectFieldCSS = css`
+  width: 30%;
+`;
