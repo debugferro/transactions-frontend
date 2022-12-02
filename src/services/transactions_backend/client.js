28ce4f3d2d73eb21175bde7b719cb6aa05b085d1
Gabriel Ferro <gabrielferroroque@hotmail.com>
2022-12-02 00:40:57 +0000

    feat: created apolloClient

---
+++ src/services/transactions_backend/client.js
@@ -0,0 +1,12 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache({
+    typePolicies: {
+      Transactions: {
+        keyFields: ["nodes", ["id"]],
+      },
+    },
+  }),
+});
