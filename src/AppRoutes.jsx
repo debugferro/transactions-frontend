94ac3c1cbc8729b39dd21bf8e53eea0fbf17f8a4
Gabriel Ferro <gabrielferroroque@hotmail.com>
2022-12-02 05:16:13 +0000

    feat: added transaction page route

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Transaction from "./pages/Transaction/Transaction";
 import TransactionsIndex from "./pages/TransactionsIndex/TransactionsIndex";
 
 export function AppRoutes() {
@@ -6,6 +7,7 @@ export function AppRoutes() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TransactionsIndex />} />
+        <Route path="/transactions/:id" element={<Transaction />} />
       </Routes>
     </BrowserRouter>
   );
